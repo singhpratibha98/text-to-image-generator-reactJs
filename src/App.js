b3089c606917ce37ab425c61f97b61f8a3aa1b29
pratibha singh <ps6977616@gmail.com>
2024-01-08 23:37:57 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [inputText, setInputText] = useState('');
+  const [imageURL, setImageURL] = useState('');
+  const API_TOKEN = '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'; 
+
+  useEffect(() => {
+    if (inputText) {
+      axios
+        .post(
+          'https://api-inference.huggingface.co/models/prompthero/openjourney-v4',
+          {
+            inputs: inputText,
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${API_TOKEN}`,
+            },
+            responseType: 'blob', // Set responseType to 'blob' to get binary data
+          }
+        )
+        .then((response) => {
+          const imageUrl = URL.createObjectURL(response.data);
+          setImageURL(imageUrl);
+        })
+        .catch((error) => {
+          console.error('Error generating image:', error);
+          setImageURL('Error generating image:');
+        });
+    }
+  }, [inputText]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display:"flex", justifyContent:'center' , paddingTop:"50px",minHeight:"100vh", backgroundColor:"pink" , backgroundSize:"cover" }}>
+      <div style={{display:"flex", flexDirection:"column", alignItems:"center"}}>
+        <h1  >Image Generation App</h1>
+        <form>
+          <input style={{padding: "10px", color:"orange"}}
+            type="text"
+            placeholder="Enter text..."
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+          />
+        </form>
+        
+        {imageURL && (
+          <div >
+            <img width={400} src={imageURL} alt="Generated" style={{marginTop:"40px"}} />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
